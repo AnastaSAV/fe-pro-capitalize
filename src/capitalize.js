a9dfb2d5b59dbbbbc06cbafc0b4b3b61d4ef7de9
AnastaSAV <anastasia.personalbox@gmail.com>
2021-12-21 13:41:32 +0200

    added js

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,12 @@
+const string = "test test test";
 function capitalize(string) {
+	let arr = string.split(' ');
+	let newArr = arr.map(function (word) {
+		return word[0].toUpperCase() + word.substring(1);
+	})
+	let result = newArr.join(' ');
+	return result;
 }
+console.log(capitalize(string));
 
 module.exports = capitalize;
--- src/checks.js
+++ src/checks.js
@@ -1,5 +1,4 @@
 const string = "test test test";
 // проверяем как угодно тут
 
-
 console.log(capitalize(string)); // Test Test Test
